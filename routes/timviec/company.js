8e23082d0e316277e24a979ec66d00f563e9b43e
Cuong <huucuongyd@gmail.com>
2023-05-05 17:01:59 +0700

    api Login for candidate

--- routes/timviec/company.js
+++ routes/timviec/company.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var company = require('../../controllers/timviec/company');
-var formData=require('express-form-data')
+var formData = require('express-form-data')
 const uploadAvatarUser=require('../../services/functions')
+
 router.post('/register',uploadAvatarUser.upload.single('avatarUser'),company.register);
 
 module.exports = router;
