0981309d8927d895429afdb7d06ec01e666aabbd
duongtunglam192 <71377794+duongtunglam192@users.noreply.github.com>
2023-05-09 19:30:12 +0700

    .

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var companyRouter = require('./routes/timviec/company');
 var toolAddDataRouter = require('./routes/tools');
 
 var app = express();
-
+//
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
