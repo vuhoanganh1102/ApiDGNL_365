a702d9c0862ea008bb5bd2a98c571a2791a1f431
unknown <tinhnvt1610@email.com>
2023-05-30 17:32:17 +0700

    vanthu/dexuat 3

--- routes/vanthu/van_ban.js
+++ routes/vanthu/van_ban.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const vanBan = require('../../controllers/tools/vanthu');
 
 router.get('/Vanban', vanBan.tool_VanBan);
+router.post('/Vanban/list ',fo vanBan.tool_VanBan);
+router.post('/listNewHot', formData.parse(), newTV365.listNewHot)
+
 module.exports = router;
