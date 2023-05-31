c29667722cf16909b1aa07641b413b494eec0d52
unknown <tinhnvt1610@email.com>
2023-05-31 08:04:41 +0700

    .

--- routes/vanthu/van_ban.js
+++ routes/vanthu/van_ban.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const vanBan = require('../../controllers/tools/vanthu');
+// const vanBan = require('../../controllers/tools/vanthu');
 
-router.get('/Vanban', vanBan.tool_VanBan);
-router.post('/Vanban/list ',fo vanBan.tool_VanBan);
-router.post('/listNewHot', formData.parse(), newTV365.listNewHot)
+// router.get('/Vanban', vanBan.tool_VanBan);
+// router.post('/Vanban/list ',fo vanBan.tool_VanBan);
+// router.post('/listNewHot', formData.parse(), newTV365.listNewHot)
 
 module.exports = router;
