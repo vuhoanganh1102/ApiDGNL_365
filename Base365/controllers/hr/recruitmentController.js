d196d06084acb3819047205d5c5a5c04530cff26
dungPtitt <lvdfullstack@gmail.com>
2023-06-15 18:28:30 +0700

    [HR]update api recruitment

--- Base365/controllers/hr/recruitmentController.js
+++ Base365/controllers/hr/recruitmentController.js
@@ -16,17 +16,14 @@ exports.getListRecruitment= async(req, res, next) => {
         pageSize = Number(req.body.pageSize);
         const skip = (page - 1) * pageSize;
         const limit = pageSize;
-        let listCondition = {};
+        
+        let listCondition = {comId: req.comId};
 
         // dua dieu kien vao ob listCondition
         if(name) listCondition.name =  new RegExp(name);
-        let fieldsGet = 
-        {   
-            _id: 1,name: 1,createdBy: 1,createdAt: 1,deletedAt: 1,isDelete: 1,applyFor: 1,slug: 1,comId: 1,isCom: 1
-        }
-        const listStageRecruit = await functions.pageFindWithFields(StageRecruitment, listCondition, fieldsGet, { _id: 1 }, skip, limit); 
-        const totalCount = await functions.findCount(StageRecruitment, listCondition);
-        return functions.success(res, "Get list stage recruitment success", {totalCount: totalCount, data: listStageRecruit });
+        const listRecruit = await functions.pageFind(Recruitment, listCondition, { _id: 1 }, skip, limit); 
+        const totalCount = await functions.findCount(Recruitment, listCondition);
+        return functions.success(res, "Get list recruitment success", {totalCount: totalCount, data: listRecruit });
     } catch (e) {
         console.log("Err from server", e);
         return functions.setError(res, "Err from server", 500);
@@ -40,29 +37,30 @@ exports.createRecruitment = async(req, res, next) => {
             return functions.setError(res, "Missing input value!", 404);
         }
         let slug = hrService.titleToSlug(nameProcess);
-        console.log(slug);
         //tao quy trinh
+        // let createBy = req.infoLogin?'Công ty' : infoLogin.name
         let recruitment = new Recruitment({
             // id: 11,
             name: nameProcess,
-            createdBy: "Cong ty",
+            createdBy: 'Công ty',
             createdAt: Date.now(),
             applyFor: applyFor,
+            slug: slug,
             comId: comId,
             isCom: 1
         });
-        // recruitment = await recruitment.save();
-        // console.log(recruitment);
+        recruitment = await recruitment.save();
+        console.log(recruitment);
 
         //tao cac giai doan cua quy trinh do
         let stageRecruit = new StageRecruitment({
             id: 1,
-            recruitmentId: 1,
+            recruitmentId: recruitment.id,
             name: nameStage,
             positionAssumed: posAssum,
             target: target,
             completeTime: time,
-            description: encodedDes
+            description: Buffer.from(des, 'base64')
         });
         // await StageRecruitment.create(stageRecruit);
         return functions.success(res, 'Create recruitment success!');
@@ -92,22 +90,17 @@ exports.updateRecruitment = async(req, res, next) => {
     }
 }
 
-exports.updateStatusDeleteRecruitment = async(req, res, next) => {
+exports.softDeleteRecruitment = async(req, res, next) => {
     try {
-        let recruitId = Number(req.query.recruitId);
-        let recruit = await Recruitment.findOne({id: recruitId}, {isDelete: 1});
-        if(!recruit) {
-            return functions.setError(res, "Recruitment not found!", 505);
-        }
-        let isDelete = 0;
-        if(recruit.isDelete == 0) isDelete=1;
-
-        //
-        await Recruitment.findOneAndUpdate({id: recruitId}, {
+        let recruitmentId = req.query.recruitmentId;
+        let recruitment = await Recruitment.findOneAndUpdate({id: recruitmentId}, {
             deletedAt: Date.now(),
-            isDelete: isDelete
+            isDelete: 1
         })
-        return functions.success(res, "Delete temporary recruitment success!");
+        if(!recruitment) {
+            return functions.setError(res, "News not found!", 505);
+        }
+        return functions.success(res, "Soft delete recruitment success!");
     } catch (e) {
         console.log("Error from server", e);
         return functions.setError(res, "Error from server", 500);
@@ -128,5 +121,141 @@ exports.deleteRecruitment = async(req, res, next) => {
     } catch (e) {
         console.log("Error from server", e);
         return functions.setError(res, "Error from server", 500);
+    }
+}
+
+//------------------------------controller recruitment new----------------------
+exports.getListRecruitmentNews= async(req, res, next) => {
+    try {
+        let {page, pageSize, title, fromDate, toDate} = req.body;
+
+        //id company lay ra sau khi dang nhap
+        let comId = req.comId;
+        if(!page || !pageSize){
+            return functions.setError(res, "Missing input page or pagesize", 401);
+        }
+        
+        page = Number(page);
+        pageSize = Number(pageSize);
+        const skip = (page - 1) * pageSize;
+        const limit = pageSize;
+        let listCondition = {isDelete: 0};
+        // dua dieu kien vao ob listCondition
+        if(title) listCondition.title =  new RegExp(title);
+        if(comId) listCondition.comId =  Number(comId);
+        if(fromDate) listCondition.timeStart = {$gte: fromDate};
+        if(toDate) listCondition.timeEnd = {$lte: toDate};
+        console.log(listCondition);
+
+        const listRecruitmentNews = await functions.pageFind(RecruitmentNews, listCondition, { _id: 1 }, skip, limit);
+        const totalCount = await functions.findCount(RecruitmentNews, listCondition);
+        return functions.success(res, "get list blog success", {totalCount: totalCount, data: listRecruitmentNews });
+    } catch (e) {
+        console.log("Err from server", e);
+        return functions.setError(res, "Err from server", 500);
+    }
+}
+
+
+
+exports.checkDataRecruitmentNews = async(req, res, next) => {
+    try {
+        let {
+                title,posApply,cityId,address,cateId,salaryId,number,timeStart,timeEnd,jobDetail,wokingForm,probationaryTime,moneyTip,
+                jobDes,interest,recruitmentId,jobExp,degree,gender,jobRequire,memberFollow,hrName,comId,isCom,createdBy
+            } = req.body;
+        let fields = [
+                title,posApply,cityId,cateId,salaryId,number,timeStart,timeEnd,jobDetail,wokingForm,
+                jobDes,interest,recruitmentId,jobExp,degree,gender,jobRequire,memberFollow,hrName
+            ]
+        for(let i=0; i<fields.length; i++){
+            if(!fields[i])
+                return functions.setError(res, `Missing input value ${i+1}`, 404);
+        }
+        // them cac truong muon them hoac sua
+        req.info = {
+            title: title,
+            posApply: posApply,
+            cityId: cityId,
+            address: address,
+            cateId: cateId,
+            salaryId: salaryId,
+            number: number,
+            timeStart: timeStart,
+            timeEnd: timeEnd,
+            jobDetail: jobDetail,
+            wokingForm: wokingForm,
+            probationaryTime: probationaryTime,
+            moneyTip: moneyTip,
+            jobDes: jobDes,
+            interest: interest,
+            recruitmentId: recruitmentId,
+            jobExp: jobExp,
+            degree: degree,
+            gender: gender,
+            jobRequire: jobRequire,
+            memberFollow: memberFollow,
+            hrName: hrName
+        }
+        return next();
+    } catch (e) {
+        console.log("Err from server!", e);
+        return functions.setError(res, "Err from server!", 500);
+    }
+}
+
+exports.createRecruitmentNews = async(req, res, next) => {
+    try {
+        //lay thong tin tu nguoi dung nhap
+        let fields = req.info;
+
+        //them cac truong an
+        fields.createdAt = Date.now();
+        fields.comId = req.comId;
+        fields.createBy = 'Công ty';
+        fields.isCom = 1;
+        console.log(req.comId);
+        //tao 
+        // let recruitmentNews = new RecruitmentNews(fields);
+        // await recruitmentNews.save();
+        return functions.success(res, 'Create recruitmentNews RN365 success!');
+    } catch (e) {
+        console.log("Err from server!", e);
+        return functions.setError(res, "Err from server!", 500);
+    }
+}
+
+exports.updateRecruitmentNews = async(req, res, next) => {
+    try{
+        let recruitmentNewsId = req.body.recruitmentNewsId;
+        if(!recruitmentNewsId)
+            return functions.setError(res, "Missing input id news!", 404);
+        let fields = req.info;
+        fields.updatedAt = Date.now();
+        let recruitmentNews = await RecruitmentNews.findOneAndUpdate({id: recruitmentNewsId}, fields);
+        if(!recruitmentNews) {
+            return functions.setError(res, "Recruitment News not found!", 505);
+        }
+        return functions.success(res, "Update news success!");
+    }catch(err){
+        console.log("Err from server!", err);
+        return functions.setError(res, "Err from server!", 500);
+    }
+}
+
+exports.softDeleteRecuitmentNews = async(req, res, next) =>{
+    try{
+        let newsId = req.query.newsId;
+        let recruitmentNews = await RecruitmentNews.findOneAndUpdate({id: newsId}, {
+            deletedAt: Date.now(),
+            isDelete: 1
+        })
+        if(!recruitmentNews) {
+            return functions.setError(res, "News not found!", 505);
+        }
+        return functions.success(res, "Soft delete news success!");
+    }catch(err){
+        console.log("Err from server!", err);
+        return functions.setError(res, "Err from server!", 500);
     }
 }
--- Base365/routes/hr/recruitmentRoute.js
+++ Base365/routes/hr/recruitmentRoute.js
@@ -3,15 +3,37 @@ var router = express.Router();
 var recruitmentController = require('../../controllers/hr/recruitmentController');
 var formData = require('express-form-data');
 const functions = require('../../services/functions');
+const hrService = require('../../services/hr/hrService');
 
 //api lay ra danh sach recruitmentController theo cac truong
-router.post('/getRecruit', formData.parse(), recruitmentController.getListRecruitment);
-router.post('/', formData.parse(), recruitmentController.createRecruitment);
-router.put('/', formData.parse(), recruitmentController.updateRecruitment);
+router.post('/getRecruit', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.getListRecruitment);
+
+//
+router.post('/', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.createRecruitment);
+
+//
+router.put('/', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.updateRecruitment);
 
 //xoa quy trinh tuyen dung vao muc da xoa gan day
-router.put('/statusDelete', formData.parse(), recruitmentController.updateStatusDeleteRecruitment);
+router.put('/softDelete', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.softDeleteRecruitment);
 //xoa vinh vien
-router.delete('/', formData.parse(), recruitmentController.deleteRecruitment);
+router.delete('/', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.deleteRecruitment);
+
+//-------------------------------api tin tuyen dung
+
+//lay ra ds tin va tim kiem tin
+router.post('/listNews', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.getListRecruitmentNews);
+
+//them moi tin tuyen dung
+router.post('/news', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.checkDataRecruitmentNews, recruitmentController.createRecruitmentNews);
+
+//sua
+router.put('/news', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.checkDataRecruitmentNews, recruitmentController.updateRecruitmentNews);
+
+//xoa tam thoi
+router.put('/softDeleteNews', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.softDeleteRecuitmentNews);
+
+//-------------------------------api ung vien
+router.post('/candi', formData.parse(), hrService.HR_CheckTokenCompany, recruitmentController.getListRecruitmentNews);
 
 module.exports = router;
