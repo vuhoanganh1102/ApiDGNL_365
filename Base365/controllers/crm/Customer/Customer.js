e8cc731c63155eee58d14be9657a6a571685e4ea
lamnguyen1603 <nguyenlam2521603@gmail.com>
2023-06-16 08:05:15 +0700

    bau dau ngay 16/6

--- Base365/controllers/crm/Customer/Customer.js
+++ Base365/controllers/crm/Customer/Customer.js
@@ -5,7 +5,7 @@ const functions = require("../../../services/functions");
 
 exports.addCustomer = async (req,res) => {
     try {
-       let {email,phone_number,name,stand,logo,birthday,tax_code,cit_id,district_id,ward,address} = req.body
+       let {name,stand,logo,birthday,tax_code,cit_id,district_id,ward,address} = req.body
     }catch (error) {
     res.status(500).json({ error: 'Đã xảy ra lỗi trong quá trình xử lý.' });
   }
