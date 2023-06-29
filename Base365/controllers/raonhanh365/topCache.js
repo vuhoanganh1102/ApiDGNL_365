ac1879db251adb3faf04ab3432b8b3944664174a
cuong <cuongbatbai2001@gmail.com>
2023-06-29 08:57:50 +0700

    update

--- Base365/controllers/raonhanh365/topCache.js
+++ Base365/controllers/raonhanh365/topCache.js
@@ -12,87 +12,84 @@ exports.getTopCache = async (req, res, next) => {
 
         let cateId = req.body.cateId;
 
-        let city2 = await City.find({}, { _id: 1, name: 1, parentId: 1 });
 
-        let city3 = await City.find({ parentId: { $ne: 0 } }, { _id: 1, name: 1, parentId: 1 });
+        // let city2 = await City.find({}, { _id: 1, name: 1, parentId: 1 });
 
-        let db_cat = await category.find({ active: 1 })
+        // let city3 = await City.find({ parentId: { $ne: 0 } }, { _id: 1, name: 1, parentId: 1 });
 
-        let db_cat1 = await category.find({ active: 1, parentId: 0 })
+        // let cat = await category.find({ active: 1 })
 
-        let tags_tk = await Tags.find({}, { _id: 1, name: 1 })
+        // let cat1 = await category.find({ active: 1, parentId: 0 })
 
-        let db_catvl = await CateVl.find({ active: 1 }, { _id: 1, name: 1 })
+        // let tagsTk = await Tags.find({}, { _id: 1, name: 1 })
 
-        let db_tkcatvl = [];
+        // let catVl = await CateVl.find({ active: 1 }, { _id: 1, name: 1 })
 
-        let db_tkcat = [];
+        // let ward = await PhuongXa.find();
 
-        let db_pxa = await PhuongXa.find();
+        // let allCate = await category.find({ _id: { $nin: [119, 120] }, active: 1 });
 
-        let db_all_dmuc = await category.find({ _id: { $nin: [119, 120] }, active: 1 });
+        // let allCateChild = await category.find({ active: 1, _id: { $nin: [1, 2, 3, 13, 18, 20, 21, 22, 23, 25, 51, 119, 74, 77, 93] } })
 
-        let db_all_dmuc_con = await category.find({ active: 1, _id: { $nin: [1, 2, 3, 13, 18, 20, 21, 22, 23, 25, 51, 119, 74, 77, 93] } })
+        // let allCate1 = await category.find({ active: 1, _id: { $nin: [119, 121, 19, 24] } })
 
-        let db_all_dmuc1 = await category.find({ active: 1, _id: { $nin: [119, 121, 19, 24] } })
-
-        let manHinhTheoDanhMuc = await CateDetail.findById(cateId,{screen:1})
+        let screen = await CateDetail.findById(cateId,{screen:1})
         
-        let nhomSanPham = await CateDetail.findById(cateId,{productGroup:1})
+        let productGroup = await CateDetail.findById(cateId,{productGroup:1})
 
-        let nhomSanPhamChatLieu = await CateDetail.findById(cateId,{productMaterial:1})
+        let productMaterial = await CateDetail.findById(cateId,{productMaterial:1})
 
-        let nhomSanPhamHinhDang = await CateDetail.findById(cateId,{productShape:1})
+        let productShape = await CateDetail.findById(cateId,{productShape:1})
 
-        let giongThuCung = await CateDetail.findById(cateId,{petPurebred:1})
+        let petPurebred = await CateDetail.findById(cateId,{petPurebred:1})
 
-        let thongTinThuCung = await CateDetail.findById(cateId,{petInfo:1})
+        let petInfo = await CateDetail.findById(cateId,{petInfo:1})
 
-        let xuatXu = await CateDetail.findById(cateId,{origin:1})
+        let origin = await CateDetail.findById(cateId,{origin:1})
 
-        let dungLuong  = await CateDetail.findById(cateId,{capacity:1})
+        let capacity  = await CateDetail.findById(cateId,{capacity:1})
 
-        let hang = await  CateDetail.findById(cateId,{brand:1})
+        let brand = await  CateDetail.findById(cateId,{brand:1})
 
-        let mauSac = await  CateDetail.findById(cateId,{colors:1})
+        let colors = await  CateDetail.findById(cateId,{colors:1})
 
-        let boViSuLi = await CateDetail.findById(cateId,{processor:1})
+        let processor = await CateDetail.findById(cateId,{processor:1})
 
-        let monTheThao = await CateDetail.findById(cateId,{sport:1})
+        let sport = await CateDetail.findById(cateId,{sport:1})
 
-        let tangPhong = await  CateDetail.findById(cateId,{storyAndRoom:1})
+        let storyAndRoom = await  CateDetail.findById(cateId,{storyAndRoom:1})
 
-        let baoHanh = await  CateDetail.findById(cateId,{warranty:1})
+        let warranty = await  CateDetail.findById(cateId,{warranty:1})
 
-        let namSanXuat = await  CateDetail.findById(cateId,{yearManufacture:1})
-        data.arrcity2 = city2;
-        data.arrcity3 = city3;
-        data.db_cat = db_cat;
-        data.db_cat1 = db_cat1;
-        data.tags_tk = tags_tk;
-        data.db_catvl = db_catvl;
-        data.db_tkcatvl = db_tkcatvl;
-        data.db_tkcat = db_tkcat;
-        data.db_pxa = db_pxa;
-        data.db_all_dmuc = db_all_dmuc;
-        data.db_all_dmuc_con = db_all_dmuc_con;
-        data.db_all_dmuc1 = db_all_dmuc1;
-        data.manHinhTheoDanhMuc = manHinhTheoDanhMuc;
-        data.nhomSanPham = nhomSanPham;
-        data.nhomSanPhamChatLieu = nhomSanPhamChatLieu;
-        data.nhomSanPhamHinhDang = nhomSanPhamHinhDang;
-        data.giongThuCung = giongThuCung;
-        data.thongTinThuCung = thongTinThuCung;
-        data.xuatXu = xuatXu;
-        data.dungLuong = dungLuong;
-        data.hang = hang;
-        data.mauSac = mauSac;
-        data.boViSuLi = boViSuLi;
-        data.monTheThao = monTheThao;
-        data.tangPhong = tangPhong;
-        data.baoHanh = baoHanh;
-        data.namSanXuat = namSanXuat;
+        let yearManufacture = await  CateDetail.findById(cateId,{yearManufacture:1})
 
+        let allType = await CateDetail.findById(cateId,{allType:1})
+        // data.arrcity2 = city2;
+        // data.arrcity3 = city3;
+        // data.cat = cat;
+        // data.cat1 = cat1;
+        // data.tagsTk = tagsTk;
+        // data.catVl = catVl;
+        // data.ward = ward;
+        // data.allCate = allCate;
+        // data.allCateChild = allCateChild;
+        // data.allCate1 = allCate1;
+        data.screen = screen;
+        data.productGroup = productGroup;
+        data.productMaterial = productMaterial;
+        data.productShape = productShape;
+        data.petPurebred = petPurebred;
+        data.petInfo = petInfo;
+        data.origin = origin;
+        data.capacity = capacity;
+        data.brand = brand;
+        data.colors = colors;
+        data.processor = processor;
+        data.sport = sport;
+        data.storyAndRoom = storyAndRoom;
+        data.warranty = warranty;
+        data.yearManufacture = yearManufacture;
+        data.allType = allType;
         return functions.success(res, 'get data success', { data })
     } catch (error) {
         console.log("🚀 ~ file: topCache.js:12 ~ exports.getTopCache= ~ error:", error)
