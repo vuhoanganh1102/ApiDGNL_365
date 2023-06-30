2a872ce08b5c2f9d071d29cfb07f47e85bb4f6c1
lamnguyen1603 <nguyenlam2521603@gmail.com>
2023-06-30 09:12:49 +0700

    up lai van thu

--- Base365/routes/vanthu/DeXuat/edit_deXuat.js
+++ Base365/routes/vanthu/DeXuat/edit_deXuat.js
@@ -8,7 +8,7 @@ const functions = require('../../../services/functions')
 router.post('/edit_delType',functions.checkToken,data.parse(), edit_Controller.edit_del_type);
 
 
-router.post('/edit_active',data.parse(), edit_Controller.edit_active);
+router.post('/edit_active',functions.checkToken,data.parse(), edit_Controller.edit_active);
 
 //router.post('/edit_time_tiep_nhan', data.parse(), edit_Controller.edit_tiep_nhan);
 module.exports = router;
