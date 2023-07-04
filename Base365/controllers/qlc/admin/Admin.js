191fc42cf2c33047fb55fe3ae4de7ae4c42ca969
Trunghere <trunghere@gmail.com>
2023-07-04 10:52:12 +0700

    4/7-update API-site + admin QLC

--- Base365/controllers/qlc/admin/Admin.js
+++ Base365/controllers/qlc/admin/Admin.js
@@ -1,22 +1,41 @@
 const functions = require("../../../services/functions")
 const user = require("../../../models/Users")
+const md5 = require('md5');
 
 //cai dat dich vu Vip
 exports.setVip = async (req, res) => {
-    let idQLC = req.body.idQLC
+    try{
+    let idQLC = req.user.data.idQLC
     let com_ep_vip = req.body.com_ep_vip
     let com_vip_time = req.body.com_vip_time
-
-    if(com_ep_vip < 5 ){
-        functions.setError(res,"số lượng nhân viên được đăng kí vip không được dưới 5")
+    let com_id = req.body.com_id
+    let now = new Date()
+    // console.log(Date.parse(com_vip_time))
+    if(!com_id){
+        functions.setError(res,"vui lòng nhập id công ty")
+    } else if(com_ep_vip < 5 ){
+        await functions.setError(res,"số lượng nhân viên được đăng kí vip không được dưới 5")
     }else if(isNaN(com_ep_vip)){
         functions.setError(res,"số lượng nhân viên phải là số")
+    }else if(now > new Date(com_vip_time)){
+        await functions.setError(res,"số ngày nhập phải lớn hơn hiện tại ")
     }else{
-        let data = await user.findOneAndUpdate({idQLC : idQLC , type : 1 },{
-            $set : {
-                
-            }
-        })
+        let find = await user.findOne({idQLC: com_id, type :1}).lean()
+        if(!find){
+            await functions.setError(res," không tìm thấy công ty ")
+        }else{
+            let data = await user.updateOne({idQLC : com_id , type : 1 },{
+                $set : {
+                    'inForCompany.cds.com_vip' : 1,
+                    'inForCompany.cds.com_ep_vip' : com_ep_vip,
+                    'inForCompany.cds.com_vip_time' : Date.parse(com_vip_time),
+                }
+            })
+            await functions.success(res , "cập nhật thành công ", {data})
+        }
+    }
+    }catch(e){
+        functions.setError(res, e.message)
     }
 }
 
@@ -50,9 +69,9 @@ exports.getList= async (req,res) =>{
             let checkNew = new Date(inputNew)
             let checkOld = new Date(inputOld)
             const currentDate = new Date();
-            const year = currentDate.getFullYear();
-            const month = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0, nên cần cộng thêm 1
-            const day = currentDate.getDate()  ;
+            // const year = currentDate.getFullYear();
+            // const month = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0, nên cần cộng thêm 1
+            // const day = currentDate.getDate()  ;
             
             // const formattedDate = `${year}-${month}-${day}`;
 
@@ -94,7 +113,7 @@ exports.getList= async (req,res) =>{
             console.log(listCondition)
              data = await user.find(listCondition).select('com_id userName Email phoneTK address fromWeb createdAt status_com authentic inForCompany.cds.com_vip inForCompany.cds.com_ep_vip inForCompany.cds.com_vip_time ').skip((pageNumber - 1) * 25).limit(25).sort({ _id : -1});
              console.log(data)
-             if (data === []) {
+             if (data===[]) {
                 //  let vip = data[0].inForCompany.cds.com_vip
                 //  let EpVip = data[0].inForCompany.cds.com_ep_vip
                 //  let comVipTime = data[0].inForCompany.cds.com_vip_time
@@ -121,6 +140,50 @@ exports.getList= async (req,res) =>{
 
  
 }
-exports.getListComVip = async (req, res)=>{
+// exports.getListFeedback = async (req, res)=>{
+//     try{
+//         const idQLC = req.user.data.idQLC
+//         const pageNumber = req.body.pageNumber || 1;
+//         const request = req.body;
+//         let 
+//     }catch(e){
+//         functions.setError(res, e.message)
+//     }
 
+// }
+exports.updatePassword = async (req, res, next) => {
+    try {
+        let idQLC = req.user.data.idQLC
+        let password = req.body.password;
+        let com_id = req.body.com_id;
+        if(password&&com_id){
+            let checkPassword = await functions.verifyPassword(password)
+            if (checkPassword) {
+                return functions.setError(res, "sai dinh dang Mk", 404)
+            }
+            if(password.length < 6){
+                return functions.setError(res, 'Password quá ngắn', 400)
+            }
+            let find = await user.findOne({idQLC: com_id, type :1}).lean()
+            if(!find){
+                await functions.setError(res," không tìm thấy công ty ")
+            }else{
+                        await user.updateOne({ idQLC: com_id, type :1 }, {
+                            $set: {
+                                password: md5(password),
+                            }
+                        });
+                        return functions.success(res, 'cập nhập thành công')
+                 }    
+                
+        }else{
+            return functions.setError(res, "nhập thiếu thông tin", 404)
+
+        }
+
+
+        } catch (error) {
+        console.log(error)
+        return functions.setError(res, error)
+    }
 }
