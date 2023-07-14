bd9a7aa7e299205ff4f3d7d87ffb5e8ecad9b284
Trunghere <trunghere@gmail.com>
2023-07-14 15:38:28 +0700

    edit model

--- Base365/models/qlc/CalendarWorkEmployee.js
+++ Base365/models/qlc/CalendarWorkEmployee.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CalendarWorkEmployee = new Schema({
+const Employee_cycle = new Schema({
     epcy_id: {//ID danh sách nhân viên có lịch làm việc
         type: Number,
         required: true
     },
-    idQLC: {
+    ep_id: {
         type: Number
     },
     cy_id: {//ID lịch làm việc
         type: Number
     },
     update_time: {//thời điểm áp dụng
-        type: Date
+        type: Date,
+        default : new Date(),
     },
 
 })
 
-module.exports = mongoose.model('QLC_Employee_cycle', CalendarWorkEmployee);
+module.exports = mongoose.model('QLC_Employee_cycle', Employee_cycle);
