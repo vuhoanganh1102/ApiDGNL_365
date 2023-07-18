7bae2ad9fe12b94b66f736c34c26871281233373
lamnguyen1603 <nguyenlam2521603@gmail.com>
2023-07-18 14:24:07 +0700

    chua day router

--- Base365/routes/vanthu/GuiNhanCongVan/homeRoute.js
+++ Base365/routes/vanthu/GuiNhanCongVan/homeRoute.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const homeController = require("../../../controllers/vanthu/GuiNhanCongVan/homeController");
 var formData = require('express-form-data');
 const functions = require('../../../services/functions');
-const vanThuService = require('../../../services/vanThu');
+const vanThuService = require('../../../services/vanthu');
 
-router.post('/getTotalVanBan', vanThuService.checkToken, formData.parse(), homeController.getTotalVanBan);
+router.post('/getTotalVanBan', vanThuService.checkToken,formData.parse(),homeController.getTotalVanBan);
 
 module.exports = router
