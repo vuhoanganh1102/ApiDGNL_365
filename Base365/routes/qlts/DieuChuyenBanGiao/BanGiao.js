52390c132c76adf139a4096b1bcac92155f9be4a
Trunghere <trunghere@gmail.com>
2023-07-24 11:32:29 +0700

    DC/BG  site QLTS

--- Base365/routes/qlts/DieuChuyenBanGiao/BanGiao.js
+++ Base365/routes/qlts/DieuChuyenBanGiao/BanGiao.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 const formData = require('express-form-data');
 const controllers = require('../../../controllers/qlts/dieu_chuyen_ban_giao/BanGiao');
 const functions = require('../../../services/functions');
+const fnc = require('../../../services/QLTS/qltsService')
 
 
 
-router.post('/list', functions.checkToken, formData.parse(), controllers.list);
+router.post('/list', functions.checkToken,fnc.checkRole("DC_BG",1), formData.parse(), controllers.list);
 
-router.post('/listDetailAllocation', functions.checkToken, formData.parse(), controllers.listDetailAllocation);
+router.post('/listDetailAllocation', functions.checkToken,fnc.checkRole("DC_BG",1), formData.parse(), controllers.listDetailAllocation);
 
-router.post('/listDetailRecall', functions.checkToken, formData.parse(), controllers.listDetailRecall);
+router.post('/listDetailRecall', functions.checkToken,fnc.checkRole("DC_BG",1), formData.parse(), controllers.listDetailRecall);
 
 module.exports = router;
