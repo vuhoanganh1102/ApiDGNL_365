aaa491f26721d900ff1166118d0c82107afc3e29
cuong <cuongbatbai2001@gmail.com>
2023-07-24 15:04:49 +0700

    aa

--- Base365/models/QuanLyTaiSan/Sua_chua.js
+++ Base365/models/QuanLyTaiSan/Sua_chua.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const model_Sua_chua = new mongoose.Schema({
     sc_id: {
         type: Number,
-        require: true
+        require: true,
+        unique: true
     },
     suachua_taisan: {
         type: Number,
